0494d0c61c82df11a59f342cc116e9dae33814db
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-13 18:00:26 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using the puts command to print
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
