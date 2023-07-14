63ae80fdad0f98f9e71fe496ab7a9ce7bbe0a3e6
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-14 06:37:16 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,2 @@
-#include <stdio.h>
-/**
- * main - A program that prints the size of various types on the computer
- * Description: Produce same as output
- * Return 0 (Success)
- */
-int main(void);
-{
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(a));
-printf("Sive of an integer: 4 byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: 4 byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: 8 byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: 4 byte(s)\n", (unsigned long)sizeof(e));
-return (0);
-}
+#!/bin/bash
+gcc 6-size.c
