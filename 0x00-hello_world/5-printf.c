102bd34e1b50275256d219f41edc690dad287c02
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-13 19:39:54 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-printf("A peice of art,");
-printf("but the outcome is with proper grammar.\n")
-return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
