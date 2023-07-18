25bfe37e3ce9a87fa5774b4dbbd9478c8405f218
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-18 16:11:11 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A program that prints putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("putchar\n");
+
+	return (0);
+}
