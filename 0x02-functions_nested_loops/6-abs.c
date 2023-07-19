9fda8df5778c3b4d7d767b2665bda8a9cb446105
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-19 05:09:53 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: A program that print the absolute value
+ * @n: the int to check
+ * Return: an absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
