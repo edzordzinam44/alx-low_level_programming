7578f6b4ad4b02fb378062560ed9e50f825b798e
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-21 19:25:47 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,18 +15,18 @@ void times_table(void)
 		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
-			if (y == 0)
+
+			if (z < 10)
 			{
-				_putchar(z + '0');
+				if (y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+					_putchar(z + '0');
 			}
-			if (z < 10 && y != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else if (z >= 10)
+			else
 			{
 				_putchar(';');
 				_putchar(' ');
