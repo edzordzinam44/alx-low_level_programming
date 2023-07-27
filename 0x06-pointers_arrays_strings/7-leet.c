07cb16db076003ff9231ffadb67e3e818509c10a
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-27 15:35:14 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,29 @@
-#include  "main.h"
+#include "main.h"
 
 /**
- * leet - A function that encode a string to 1337
- * @s: string to be encoded
- * Description: switch isnt used
- * Return: the resulting string
+ * leet - encode a string into 1337
+ * @n: string
+ * Description: letters should be replaced by numbers
+ * Return: Success
  */
 
-char *leet(char *s)
+char *leet(char *n)
 {
 	int i, j;
 
 	char *a = "aAeEoOtTlL";
 	char *b = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for  (j = 0; j < 10; j++)
 		{
-			if (s[i] == a[j])
+			if  (n[i] == a[j])
 			{
-				s[i] = b[j];
+				n[i] = b[j];
 			}
 		}
 	}
 
-	return (s);
+	return (n);
 }
