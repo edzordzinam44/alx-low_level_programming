8b7645696da4e340c2b8d43e24e8d27589445ea1
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-07-27 06:38:39 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include  "main.h"
+
+/**
+ * leet - A function that encode a string to 1337
+ * @s: string to be encoded
+ * Description: switch isnt used
+ * Return: the resulting string
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}
