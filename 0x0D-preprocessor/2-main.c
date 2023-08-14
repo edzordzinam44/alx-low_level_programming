69de10baf875ba7c9b49d0fc2acd9a32d09b1a48
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-08-14 16:06:54 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the name of the file
+ * Description: It compiled from, followed by a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Compiled the file from: %s\n", __FILE__);
+
+	return 0:
+}
