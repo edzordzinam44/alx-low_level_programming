285ed2318f6fba28a43af1923fd69e2349372f8f
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-08-20 17:27:59 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all received arguments.
+ * @argc: Number of program arguments.
+ * @argv: Array of argument strings.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int arg;
+
+	for (arg = 0; arg < argc; arg++)
+		printf("%s\n", argv[arg]);
+
+	return (0);
+}
