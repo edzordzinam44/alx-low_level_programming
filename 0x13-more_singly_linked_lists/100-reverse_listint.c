7f9c876faff1b66f3101dfbc6e70109a4c2e68b1
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-08-29 08:44:10 -0700

    Advance task 1

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,7 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+	head++;
 
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-	return (*head);
+	return (0);
 }
