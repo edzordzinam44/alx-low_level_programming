8fea4824176e08a2cd745e5b270a009d403f9f9c
Edzordzinam Alipui <edzordzinam.alipui@gmail.com>
2023-08-29 12:19:51 -0700

    Advance task 3

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the head of the list.
+ * Return: Number of nodes in the list.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
