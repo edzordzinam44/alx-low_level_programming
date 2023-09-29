08872399b6960ff401ecf990fb42d63faff14c6a
edzordzinam44 <edzordzinam.alipui@gmail.com>
2023-09-28 19:22:22 -0700

    Task 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,51 +1,50 @@
 #include "main.h"
 
 /**
- * _power - calculate the result of raising a base to a power.
- * @base: the base value.
- * @pow: the power value.
- * Return: the result of base raised to the power.
+ * power - Calculates the result of base raised to the power of exp.
+ * @base: The base value.
+ * @exp: The exponent value.
+ * Return: The result of base raised to the power of exp.
  */
-
-unsigned long int _power(unsigned int base, unsigned int pow)
+unsigned long int _power(unsigned int base, unsigned int exp)
 {
-	unsigned long int num;
+	unsigned long int result;
 	unsigned int j;
 
-	while (j <= pow)
+	result = 1;
+	for (j = 0; j < exp; j++)
 	{
-		num *= base;
-		j++;
+		result *= base;
 	}
 
-	return (num);
+	return (result);
 }
+
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
- *
- * Return: (void)
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int dev, result;
+	unsigned long int result, divisor;
 	char flag;
 
 	flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+	divisor = _power(2, sizeof(unsigned long int) * 8 - 1);
 
-	for (; dev != 0; dev >>= 1)
+	while (divisor != 0)
 	{
-		result = n & dev;
-		if (result == dev)
+		result = n & divisor;
+		if (result == divisor)
 		{
 			flag = 1;
 			_putchar('1');
 		}
-		else if (flag == 1 || dev == 1)
+		else if (flag == 1 || divisor == 1)
 		{
-			_putchar(0);
+			_putchar('0');
 		}
-		dev >>= 1;
+		divisor >>= 1;
 	}
 }
